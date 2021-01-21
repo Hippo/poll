ee4658826cadcfde02df362281d0d13296a0009f
Hippo <Hippah@protonmail.com>
2021-01-21 00:03:13 -0600

    Removed tabs from main

--- src/main/java/rip/hippo/poll/PollApplication.java
+++ src/main/java/rip/hippo/poll/PollApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PollApplication.class, args);
+    }
 
 }
